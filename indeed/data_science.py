186e04f89244ed4d612f85207154aa05c888e9bd
Todd Minehardt <todd.minehardt@gmail.com>
2016-06-29 17:57:59 -0500

    Initial commit.

---
+++ indeed/data_science.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Read in train_{features, salaries} files.
+train_features = pd.read_csv('train_features_2013-03-07.csv')
+train_salaries = pd.read_csv('train_salaries_2013-03-07.csv')
+
+# Join the features and salaries on jobId, inner join keeps
+# entries with job ID in both files.
+train_data = pd.merge(train_features, train_salaries, on='jobId',
+                      how='inner')
+
+# We know there are 5 entries where salary = 0, so let's get
+# rid of them.
+train_data = train_data[train_data.salary != 0]
+
+# Get rid of rows with any null values.
+train_data.dropna(how='any', inplace=True)
+
+# Some visualizations will help us identify which features are good
+# predictors of salary. We'll avoid matplotlib problems by enumerating
+# company ID, job type, degree, major, and industry.
+
+# Keep track of the mappings of names to integers.
+mapping = {}
+names = ['companyId', 'jobType', 'degree', 'major', 'industry']
+for name in names:
+    for i in enumerate(train_data[name].unique()):
+        if name not in mapping.keys():
+            mapping[name] = {i[1]: i[0]}
+        else:
+            mapping[name].update({i[1]: i[0]})
+    # Replace name with integer.
+    train_data[name] = train_data[name].replace(mapping[name])
